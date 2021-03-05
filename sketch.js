e0ada405d167cca66c08ff292067de38cd5fc17c
Vedant-Menon <74135210+Vedant-Menon@users.noreply.github.com>
2021-03-05 16:07:18 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,6 +2,8 @@
 var bg,bgimg,start,startimg
 var gameState=0
 var boy, playerimg,ground,groundimg,mainimg,mainbg;
+var obstacle1;
+
 function preload(){
 bgimg=loadImage("images/bg1.jpg")
 startimg = loadImage("images/text0.png")
@@ -9,6 +11,7 @@ playicon=loadAnimation("images/frame0.gif","images/frame1.gif","images/frame2.gi
 playerimg=loadAnimation("images/r1.png","images/r2.png","images/r3.png","images/r4.png","images/r5.png","images/r6.png","images/r7.png","images/r8.png")
 mainimg=loadImage("images/bg.png")
 groundimg = loadImage("images/race.png")
+obstacle1 = loadImage("images/h1.png")
 }
 
 
@@ -26,6 +29,25 @@ function setup() {
   play=createSprite(250,300,10,10)
   play.addAnimation("playing",playicon)
   play.scale=0.1
+
+  boy=createSprite(50,192,20,50)
+  boy.addAnimation("playing",playerimg)
+  boy.scale=0.2
+  mainbg = createSprite(300,130,600,200)
+  mainbg.addImage(mainimg)
+  mainbg.scale = 0.7;
+ // mainbg.velocityX=-3;
+  boy.scale=0.2
+     
+  ground = createSprite(200,310,400,20);
+ ground.addImage(groundimg);
+ ground.velocityX = -4;
+ ground.x = ground.width /2;
+ boy.depth=mainbg.depth
+ boy.depth=boy.depth+1
+ 
+ invisibleGround = createSprite(200,220,400,10);
+ invisibleGround.visible = false
 }
 
 function draw() {
@@ -36,8 +58,9 @@ function draw() {
    bg.visible=false;
    start.visible=false;
    play.visible=false;
-  
-   
+   mainbg.visible=false;
+  ground.visible = false;
+   boy.visible = false;
    if(keyDown("space")&&gameState===1){
      gameState=2
    }
@@ -46,29 +69,21 @@ function draw() {
 
  if(gameState===2){
    createCanvas(600,200)
-   mainbg = createSprite(300,130,600,200)
- 
+   mainbg.visible=true
+  ground.visible=true
+  boy.visible=true
    
-   boy=createSprite(50,160,20,50)
-   boy.addAnimation("running",playerimg)
-   mainbg.addImage(mainimg)
-   mainbg.scale = 0.7;
-   boy.scale=0.2
-      
-   ground = createSprite(200,300,400,20);
-  ground.addImage(groundimg);
-  ground.velocityX = -4;
-  ground.x = ground.width /2;
-
-  
-  invisibleGround = createSprite(200,190,400,10);
-  invisibleGround.visible = false
- 
+  if(keyDown("up")){
+    boy.velocityY=-10
+  }
+ boy.velocityY=boy.velocityY+0.5
   if (ground.x < 0){
     ground.x = ground.width/2;
 
   };
+
   boy.collide(invisibleGround)
+  spawnObstacles()
  }
  
  
@@ -86,6 +101,9 @@ function draw() {
      bg.visible=true
      start.visible =true;
      play.visible=true;
+     mainbg.visible=false;
+  ground.visible = false;
+   boy.visible = false;
      textSize(15)
    textStyle(BOLDITALIC)
    textFont("Algerian")
@@ -99,4 +117,9 @@ function draw() {
   
  
  }
- 
+ function spawnObstacles(){
+   if(frameCount%50===0){
+     var obstacle = createSprite(600,170,10,40)
+     obstacle.velocityX = -6;
+   }
+ }
